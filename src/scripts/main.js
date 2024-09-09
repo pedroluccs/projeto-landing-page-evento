8c5825c2f6eb6c0c2a236d4a6d0ccc264defa975
Pedro Lucas <pedro.wo@live.com>
2024-09-09 18:41:55 -0400

    estilizacao das sections e adicionado a contagem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
-console.log("ola mundo")
+    AOS.init();
+
+    const dataDoEvento = new Date("Oct 12, 2024 19:00:00");
+    const timeStampDoEvento = dataDoEvento.getTime();
+
+    const contaAsHoras = setInterval(function() {
+        const agora = new Date();
+        const timeStampAtual = agora.getTime();
+
+        const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+        const diasEmMs = 1000 * 60 * 60 * 24;
+        const horaEmMs = 1000 * 60 * 60;
+        const minutosEmMs = 1000 * 60;
+
+        const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+        const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horaEmMs);
+        const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutosEmMs);
+        const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+        document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+        if (distanciaAteOEvento < 0) {
+            clearInterval(contaAsHoras);
+            document.getElementById('contador').innerHTML = 'Evento expirado'
+        }
+    }, 1000);
